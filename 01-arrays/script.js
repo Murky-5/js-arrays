e5707b6c6fa38edb5b0660ee3aaadc382084e0ac
MirkoDD <mirkodidio111@gmail.com>
2024-12-02 22:02:56 +0100

    added:01-arrays-ex_8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -49,7 +49,14 @@ console.log(isFabioPresent)
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = [];
+
+for (let i=0; i < teachers.length; i++){
+  if (teachers[i] === 'Lewis') {
+    lewisIndex.push(i)
+    }
+}
+console.log(lewisIndex)
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
